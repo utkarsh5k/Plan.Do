8891929c778e0d4036bc420545d0f9a3cfe454c0
Utkarsh Kashyap <utkarsh.k05@gmail.com>
2016-09-21 02:26:30 +0530

    Intro+Login

---
+++ app/src/main/java/com/example/test/plando/Introduction.java
@@ -0,0 +1,23 @@
+package com.example.test.plando;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
+
+
+public class Introduction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction);
+    }
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
