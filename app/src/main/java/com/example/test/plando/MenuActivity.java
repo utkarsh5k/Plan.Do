fd14cfc0ff7e8b9744e65a45f17c150e8419099e
Utkarsh Kashyap <utkarsh.k05@gmail.com>
2016-09-24 01:59:39 +0530

    UI-Elements

--- app/src/main/java/com/example/test/plando/MenuActivity.java
+++ app/src/main/java/com/example/test/plando/MenuActivity.java
@@ -1,7 +1,12 @@
 package com.example.test.plando;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+    public void betamsg(View view)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MenuActivity.this);
+        builder.setTitle("Work in progress...");
+        builder.setMessage("This feature is currently unavailable since the app is still in development phase.");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Thankyou for your patience.", Toast.LENGTH_LONG).show();
+            }
+        });
+        AlertDialog bmsg = builder.create();
+        bmsg.show();
+    }
+    public void startToDo(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(), ToDoActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/test/plando/ToDoActivity.java
@@ -0,0 +1,29 @@
+package com.example.test.plando;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ToDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Toast.makeText(getApplicationContext(), "Functionality not implemented!", Toast.LENGTH_LONG).show();
+        return false;
+    }
+}
